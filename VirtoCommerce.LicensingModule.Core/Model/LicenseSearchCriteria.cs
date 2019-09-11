13bce2c080d49e4f2cb9d25f643b5ac0a63e4104
Andrey Nikutin <ani@virtoway.com>
2019-09-11 11:47:36 +0300

    initial migration

--- VirtoCommerce.LicensingModule.Core/Model/LicenseSearchCriteria.cs
+++
@@ -1,9 +0,0 @@
-﻿using VirtoCommerce.Domain.Commerce.Model.Search;
-
-namespace VirtoCommerce.LicensingModule.Core.Model
-{
-    public class LicenseSearchCriteria : SearchCriteriaBase
-    {
-        public string Keyword { get; set; }
-    }
-}
--- VirtoCommerce.LicensingModule.Web/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.LicensingModule.Web")]
-[assembly: AssemblyDescription("")]
